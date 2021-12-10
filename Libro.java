2604c4c865a7edb168bd18209ce9c6d9f55fbee6
youssef <youssefleon75rgmail.com>
2021-12-10 10:12:32 +0100

    01 Agregandon dos metodos getter

--- Libro.java
+++ Libro.java
@@ -20,5 +20,16 @@ public class Libro {
     {
         autor = autorLibro;
         titulo = tituloLibro;
+
+    }
+    
+    public String getAutor () {
+    return autor;
+    }
+    
+    public String getTitulo () {
+    return titulo;
     }
 }
+
+
